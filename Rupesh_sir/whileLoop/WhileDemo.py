21bc9861b7721e97ba85055ac2ce16e38ce3d784
RupeshYadavCSharp <81669954+RupeshYadavCSharp@users.noreply.github.com>
2024-04-26 20:54:28 +0530

    added by rupesh sir while demo

---
+++ Rupesh_sir/whileLoop/WhileDemo.py
@@ -0,0 +1,18 @@
+num = int(input("Enter any number ")) #123
+
+cnt = 0
+while(num != 0):
+    num = num // 10    #1 // 10 = 0
+    cnt += 1          #cnt = 3
+
+print("it is a ",cnt, " digit number ")
+
+
+
+
+
+
+
+
+
+
